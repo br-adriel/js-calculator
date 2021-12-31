dd2354aa84a2fd3924a983f64c507e84d92acc78
Adriel <adriel.fariasan@gmail.com>
2021-12-31 20:31:53 -0300

    Adiciona as funcoes de operacao

---
+++ script.js
@@ -0,0 +1,7 @@
+const somar = (a, b) => a + b;
+const subtrair = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+const dividir = (a, b) => a / b;
+const porcento = (a) => a / 100;
+const potencia = (a, b) => a ** b;
+const raiz = (a) => a ** 0.5;
