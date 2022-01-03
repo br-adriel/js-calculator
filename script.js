b3b57d206d3d19cd1bc48dcc550d845eb8640117
Adriel <adriel.fariasan@gmail.com>
2022-01-03 17:23:03 -0300

    Adiciona minisdisplay de operacoes

--- script.js
+++ script.js
@@ -35,14 +35,23 @@ btnsOperacoes.porcentagem = document.getElementById("porcento");
 btnsOperacoes.potenciacao = document.getElementById("potencia");
 btnsOperacoes.radiciacao = document.getElementById("raiz");
 
-// display das operações
+// display do input do usuario
 const display = document.getElementById("display");
 
-// funcao para pegar conteudo do display
+// funcao para pegar conteudo do display de input
 const getDisplay = () => parseFloat(display.innerText);
 
-// funcao para definir conteudo do display
+// funcao para definir conteudo do display do input
 const setDisplay = (text) => (display.innerText = `${text}`);
+
+// mini-display para mostrar operacoes
+const miniDisplay = document.getElementById("mini-display");
+
+// funcao para pegar conteudo do display de  operacoes
+const getMiniDisplay = () => parseFloat(miniDisplay.innerText);
+
+// funcao para definir conteudo do display de operacoes
+const setMiniDisplay = (text) => (miniDisplay.innerText = `${text}`);
 
 // adiciona listener para mostrar numeros apertados no display
 for (numero in btnsNumeros) {
@@ -75,11 +84,31 @@ function calcular(a, b, operador) {
   }
 }
 
+function getSimbolo(operador) {
+  switch (operador) {
+    case "mais":
+      return "+";
+    case "menos":
+      return "-";
+    case "vezes":
+      return "*";
+    case "dividir":
+      return "/";
+    case "porcento":
+      return "%";
+    case "potencia":
+      return "^";
+    case "raiz":
+      return "^ 1/2";
+  }
+}
+
 // adiciona listener de click aos botoes de operacao
 for (key in btnsOperacoes) {
   btnsOperacoes[key].addEventListener("click", (e) => {
     termo1 === null ? (termo1 = getDisplay()) : (termo2 = getDisplay());
     operador = e.currentTarget.id;
+    setMiniDisplay(`${getDisplay()} ${getSimbolo(operador)}`);
     setDisplay("0");
     if (operador === "porcento" || operador === "raiz") {
       // chamar operacao de igualdade
